114198395534c2c572564101a2e3f840c080efa2
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-06-24 16:03:19 +0200

    Update rmus formula source pin

--- Formula/rmus.rb
+++ Formula/rmus.rb
@@ -1,9 +1,9 @@
 class Rmus < Formula
   desc "Keyboard-driven terminal music player"
   homepage "https://github.com/maximilianpw/rmus"
-  url "https://github.com/maximilianpw/rmus/archive/5587e4c5900eaf11dbcbc38df92281ed34f4c7b2.tar.gz"
+  url "https://github.com/maximilianpw/rmus/archive/c4775e3c3fa5a5cee9785b1c7f545a6670c0109f.tar.gz"
   version "0.1.2"
-  sha256 "abb3dabb2fdd53eb702657bb660b6bc619ea6ba584086e84ffecdc5f46d2c9b4"
+  sha256 "daae3c1a11a3a71b09671a17befe5cd42bd18dff5144ea4eb9c688b56995bb67"
   license "MIT"
   head "https://github.com/maximilianpw/rmus.git", branch: "main"
 
